60262688cbcb563849cf0ac8dea0a50e67f93b1a
Faisal Indrianto <wwf051099@gmail.com>
2022-07-03 01:08:04 +0700

    router animation and dialog provider

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,31 @@ import { darkTheme } from 'naive-ui'
 
 <template>
   <n-config-provider :theme="darkTheme">
-    <n-message-provider>
-      <RouterView />
-    </n-message-provider>
+    <n-dialog-provider>
+      <n-message-provider>
+        <router-view v-slot="{ Component }">
+          <transition name="slide-fade" appear mode="out-in">
+            <component :is="Component" />
+          </transition>
+        </router-view>
+      </n-message-provider>
+    </n-dialog-provider>
   </n-config-provider>
 </template>
 
 <style>
+/* router animation */
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
 
+.slide-fade-leave-active {
+  transition: all 0.4s cubic-bezier(1, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
 </style>
